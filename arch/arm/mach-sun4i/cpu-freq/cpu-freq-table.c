d190831f0ffcda6e309ecdbad9a4426303c51d58
myfluxi <mvdw@arcor.de>
2012-11-04 22:42:58 +0100

    mach-sun4i: cpufreq: Correct DVFS table

--- arch/arm/mach-sun4i/cpu-freq/cpu-freq-table.c
+++ arch/arm/mach-sun4i/cpu-freq/cpu-freq-table.c
@@ -102,7 +102,7 @@ static struct cpufreq_dvfs sun4i_dvfs_table[] = {
     {.freq = 960000000,  .volt = 1400}, /* core vdd is 1.40v if cpu frequency is (912Mhz, 960Mhz]   */
     {.freq = 912000000,  .volt = 1350}, /* core vdd is 1.35v if cpu frequency is (864Mhz, 912Mhz]   */
     {.freq = 864000000,  .volt = 1300}, /* core vdd is 1.30v if cpu frequency is (624Mhz, 864Mhz]   */
-    {.freq = 624000000,  .volt = 1250}, /* core vdd is 1.25v if cpu frequency is (432Mhz, 624Mhz]   */
+    {.freq = 648000000,  .volt = 1250}, /* core vdd is 1.25v if cpu frequency is (432Mhz, 648Mhz]   */
     {.freq = 432000000,  .volt = 1250}, /* core vdd is 1.25v if cpu frequency is (0, 432Mhz]        */
     {.freq = 0,          .volt = 1000}, /* end of cpu dvfs table                                    */
 };
